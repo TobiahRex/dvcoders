955f415f1991d70a5baa425a77b14f7365eb8eef
Tobiah Rex <tobiah.rex@gmail.com>
2018-09-28 20:15:11 -0700

    modified "findCombos" to find primeCombos only.

--- findCombos.js
+++ findCombos.js
@@ -1,5 +1,31 @@
+/**
+ * Called randomly to verify number is prime.
+ *
+ * @param {integer}
+ * @return {boolean}
+ */
+const isPrime = n => {
+  // n = 3
+  let result = null;
+  if (!!n) {
+    for (let i = 2; i < n; i++) {
+      //
+      if (i !== n) {
+        if (n % i === 0) {
+          result = false;
+          break;
+        } else {
+          result = true;
+        }
+      }
+    }
+  }
+  return result;
+};
+
 const partition = (list, split, memo) => {
-  console.log("split: ", split, "list: ", list);
+  console.log("----------------------------------------");
+  console.log("split: ", split);
   // If the current list is too short - return early.
   if (list.length < 2 || split < 0) {
     return;
@@ -10,20 +36,21 @@ const partition = (list, split, memo) => {
 
   // Verify sum of LIST doesn't already exist inside memo.
   if (!(sum in memo)) {
-    memo[sum] = list;
+    if (isPrime(sum)) {
+      memo[sum] = list;
+    }
     // if it does exist and the current list is longer - then replace the sum.
   } else if (list.length > memo[sum].length) {
     memo[sum] = list;
   }
-
   // create new lists based on current split point.
-  let loList = list.slice(0, split--),
-    hiList = list.slice(split++);
+  let loList = list.slice(0, split);
+  let hiList = list.slice(split);
 
   // recursively call with new lists and new split points.
   if (split !== list.length) {
-    partition(loList, split--, memo);
-    partition(hiList, split++, memo);
+    partition(loList, split - 1, memo);
+    partition(hiList, split + 1, memo);
   } else {
     return memo;
   }
@@ -36,6 +63,8 @@ const memo = {};
   }
 */
 const list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43];
-partition(list, 0, memo);
+for (let i = 0; i < list.length; i++) {
+  partition(list, i, memo);
+}
 console.log("FINAL memo: \n", memo);
 // console.log("FINAL memo: \n", JSON.stringify(memo, null, 2));
