abf80b740bf582a6a797ff42c099418e870688ec
Tobiah Rex <tobiah.rex@gmail.com>
2018-09-28 20:00:37 -0700

    debugging "findCombos".

--- findCombos.js
+++ findCombos.js
@@ -1,29 +1,32 @@
 const partition = (list, split, memo) => {
-  if (list.length < 2) {
+  console.log("split: ", split, "list: ", list);
+  // If the current list is too short - return early.
+  if (list.length < 2 || split < 0) {
+    return;
+  }
+
+  // Generate sums for current lists.
+  let sum = list.reduce((a, b) => a + b, 0);
+
+  // Verify sum of LIST doesn't already exist inside memo.
+  if (!(sum in memo)) {
+    memo[sum] = list;
+    // if it does exist and the current list is longer - then replace the sum.
+  } else if (list.length > memo[sum].length) {
+    memo[sum] = list;
+  }
+
+  // create new lists based on current split point.
+  let loList = list.slice(0, split--),
+    hiList = list.slice(split++);
+
+  // recursively call with new lists and new split points.
+  if (split !== list.length) {
+    partition(loList, split--, memo);
+    partition(hiList, split++, memo);
+  } else {
     return memo;
   }
-  let loList = list.slice(0, split - 1),
-    hiList = list.slice(split + 1);
-
-  if (loList.length > 1) {
-    let loSum = loList.reduce((a, b) => a + b, 0);
-
-    if (!(loSum in memo) && loList.length > 1) {
-      memo[loSum] = loList;
-      partition(loList, split, memo);
-    }
-  }
-
-  if (hiList.length > 1) {
-    let hiSum = hiList.reduce((a, b) => a + b, 0);
-
-    if (!(hiSum in memo) && hiList.length > 1) {
-      memo[hiSum] = hiList;
-      partition(hiList, split++, memo);
-    }
-  }
-
-  return;
 };
 const memo = {};
 
